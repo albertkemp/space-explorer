31d676872e254a2a39f53739f745015fcc88559c
albertkemp <albertkemp@5120L-144570-M.local>
2024-07-15 17:00:30 +0200

    updated position

--- scripts/main.js
+++ scripts/main.js
@@ -130,7 +130,7 @@ $close.click(closeMap);
         {id: "#airportplanet", x: 1000, y: 5000, visible: true, gravity: 0, diameter: 2500},
         {id: "#universeedge", x: -2000, y: -2000, visible: true, gravity: 0, diameter: 23500},
         {id: "#radiopotato", x: 100, y: 200, visible: true, gravity: 0, diameter: 50},
-        {id: "#hugeplanet", x: 0, y: -4000, visible: true, gravity: 0.4, diameter: 5000},
+        {id: "#hugeplanet", x: 0, y: 0, visible: true, gravity: 0.4, diameter: 5000},
     ]; 
     
     let spinplanets = [
